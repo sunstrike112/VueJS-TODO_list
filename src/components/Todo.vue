d53b2fadd37baa769e558ecdeac152da4de14d3e
PhapTDQ <phaptran112@gmail.com>
2022-03-13 17:19:09 +0700

    Step2 Props, Directives and Template Syntax

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,16 +1,44 @@
 <template>
-  <p>Todo 1</p>
-  <p>Todo 2</p>
-  <p>Todo 3</p>
+  <TodoItem
+    v-for="todo in todos"
+    v-bind:key="todo.id"
+    v-bind:todoProps="todo"
+  />
 </template>
 
 <script>
+import { ref } from "vue"
+
+import TodoItem from "./TodoItem.vue"
+
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: "Todo",
+  name: "TodoCompoment",
+  components: {
+    TodoItem,
+  },
+  setup() {
+    const todos = ref([
+      {
+        id: 1,
+        title: "Learn Vue",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Learn Vuex",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Learn Vue Router",
+        completed: false,
+      },
+    ])
+    return {
+      todos,
+    }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
